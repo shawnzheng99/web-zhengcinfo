f19017b6095b235ce778558caffba7af967bf7d1
Shawn Zheng <shawnz@checkpay.ca>
2021-03-02 16:10:56 -0800

    rm useless import

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './Navbar';
-import Footer from './Footer';
 import Project from './Project';
 import Home from './Home';
 import About from './About';
